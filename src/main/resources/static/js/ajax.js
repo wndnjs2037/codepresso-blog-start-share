ea6218d2d4dbf7a1ab6df1f683cb3b78cb7b6f32
juwon Hwang <wndnjs2037@naver.com>
2022-03-09 19:58:06 +0900

    ajax 파일수정 more 버튼 활성화해서 3개씩 게시글 더보기 기능 만들기

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -1,5 +1,32 @@
 $(function(){
+    $("#more").click(function(){ //id가 more인 객체 클릭시
+        //console.log("clicked!"); //more 버튼 클릭시 이벤트 확인
+        var next_page = parseInt($(this).attr("current-page")) + 1; //커런트페이지의 정보를 가져와서 1을 더해 넥스트 페이지의 정보를 만드는 부분
+        // this 키워드는 클릭한 부분 그 자체의 태그를 의미한다.
+        // 여기서는 more 버튼을 의미한다고 보면 된다.
 
+        $.ajax({ //ajax 호출하기
+            method : "GET",
+            url : "/post",
+            data : {"page":next_page} //요청할 페이지의 값 보내줌
+        })
+        .done(function(response) { //ajax 호출이 완료되면 실행될 구문
+            for(var post of response) { //json 어레이에서 객체를 하나씩 꺼내와서 view에 표시
+                $('#more-posts').append("<div class=\"post-preview\">" + //id가 morepost인 영역에 append 해줌
+                    "<a href=\"#\">" +
+                    "<h2 class=\"post-title\">" +
+                    post.title +
+                    "</h2>\n" +
+                    "<h3 class=\"post-subtitle\">" +
+                    post.content +
+                    "</h3></a><p class=\"post-meta\">Posted by" +
+                    post.username +
+                    "</p></div><hr class=\"my-4\" />");
+                }
+            });
+            $(this).attr("current-page", next_page);
+
+    });
 
     $(".comment-edit").hide();
 
